84c9b818f1e874b92c47debcb34a23050cec940c
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2021-10-14 18:47:31 +0200

    Se desarrollan las tareas #12 #15 #18 para resolver la [HU1] #3

--- lib/videojuego.rb
+++ lib/videojuego.rb
@@ -1,19 +1,37 @@
 module ComparerApp
+    ##
+    # Esta clase representa la información asociada a un videojuego
+
     class Videojuego
     
-        attr_accessor :nombre, :genero, :desarrollador, :plataformas, :portada, :valoracion, :f_salida, :idiomas, :tiendas_disponibles
+        attr_reader :nombre, :genero, :desarrollador, :f_lanzamiento, :idiomas
+        attr_accessor :valoracion, :plataformas_disponibles, :tiendas_distribuidoras
         
-        # Constructor 
-        def initialize(nombre, genero, desarrollador, plataformas, portada, valoracion, f_salida, idiomas, tiendas_disponibles)
+        ##
+        # Crea un nuevo videojuego con las siguientes propiedades
+        #
+        # Parámetros:
+        # +nombre+:: título del videojuego 
+        # +genero+:: genero al que pertenece el videojuego (accion, accion-aventura, aventura, carreras, cartas, deportes, educativos, estrategia, 
+        #                      guerra, logica, lucha, preguntas, concursos, rol, terror, simulacion,realidad_aumentada)
+        # +desarrollador+:: programador o compañía que ha elaborado el videojuego
+        # +plataformas_disponibles+:: medios en los que está soportado el  videojuego (PC, Smartphone, Tablet, PlayStation, Wii, Xbox, Nintendo)
+        # +valoracion+:: puntuación media de los usuarios
+        # +f_lanzamiento+:: fecha en la que el videojuego se comercializó
+        # +idiomas+:: idiomas disponibles
+        # +tiendas_distribuidoras+:: lista de tiendas que venden el videojuego
+        def initialize(nombre, genero, desarrollador, plataformas_disponibles, valoracion, f_lanzamiento, idiomas, tiendas_distribuidoras)
             @nombre = nombre
             @genero = genero
             @desarrollador = desarrollador
-            @plataformas = plataformas
-            @portada = portada
             @valoracion = valoracion
-            @f_salida = f_salida
+            if (@valoracion.between(0,10))
+                raise StandardError, "El valor de la valoración debe estar en el rango 0-10"
+            end
+            @plataformas_disponibles = plataformas_disponibles
+            @f_lanzamiento = f_lanzamiento
             @idiomas = idiomas
-            @tiendas_disponibles = tiendas_disponibles
+            @tiendas_distribuidoras = tiendas_distribuidoras
         end
     end
 end
