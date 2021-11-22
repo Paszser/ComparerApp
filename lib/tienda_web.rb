eb7790c20a7f4b8518b7e25ada20dba20f89c6c9
Paszser <dbpaszser@gmail.com>
2021-11-22 18:22:16 +0100

    Modifico tienda_web.rb avanzando #22 , y por ende, #3

--- lib/tienda_web.rb
+++ lib/tienda_web.rb
@@ -1,21 +1,19 @@
 module ComparerApp
+require_relative 'videojuego.rb'
     ##
     # Esta clase representa la información asociada a una tienda
 
     class TiendaWeb
 
         attr_reader :nombre
-        attr_accessor :precio_videojuegos
 
         ##
         # Crea una nueva tienda web con las siguientes propiedades
         # 
         # Parámetros:
-        # +nombre+:: nombre de la tienda web
-        # +precio_videojuegos+:: lista de elementos clave-valor (nombre del videojuego - precio)             
-        def initialize(nombre, precio_videojuegos)
+        # +nombre+:: nombre de la tienda web            
+        def initialize(nombre)
             @nombre = nombre
-            @precio_videojuegos = precio_videojuegos
         end
     end
 end
--- lib/videojuego.rb
+++ lib/videojuego.rb
@@ -5,7 +5,7 @@ module ComparerApp
     class Videojuego
     
         attr_reader :nombre, :genero
-        attr_accessor :valoracion, :tiendas_distribuidoras
+        attr_accessor :valoracion, :tiendas_distribuidoras, :precios_videojuego
         
         ##
         # Crea un nuevo videojuego con las siguientes propiedades
@@ -16,14 +16,33 @@ module ComparerApp
         #                      guerra, logica, lucha, preguntas, concursos, rol, terror, simulacion,realidad_aumentada)
         # +valoracion+:: puntuación media de los usuarios
         # +tiendas_distribuidoras+:: lista de tiendas que venden el videojuego
-        def initialize(nombre, genero, desarrollador, plataformas_disponibles, valoracion, f_lanzamiento, idiomas, tiendas_distribuidoras)
+        # +precios+:: lista de elementos clave-valor (nombre de la tienda - precio)
+        def initialize(nombre, genero, desarrollador, plataformas_disponibles, valoracion, f_lanzamiento, idiomas, tiendas_distribuidoras, precios_videojuego)
             @nombre = nombre
             @genero = genero
             @valoracion = valoracion
-            if (@valoracion.between(0,10))
+            if !(@valoracion.between?(0,10))
                 raise StandardError, "El valor de la valoración debe estar en el rango 0-10"
             end
             @tiendas_distribuidoras = tiendas_distribuidoras
+            @precios_videojuego = precios_videojuego
+        end
+
+        def get_lista_precios()
+            precios_videojuego = self.precios_videojuego
+            precios_videojuego = precios_videojuego.sort_by{|tienda, precio| precio}
+            precios_videojuego.each do |llave, valor|
+                puts "#{llave}: #{valor}"
+            end
         end
     end
+
+    @precios = {
+        "Game" => 30,
+        "Instant Gaming" => 5, 
+        "PSStore" => 25,
+        "Amazon" => 20
+    }
+    videoj = Videojuego.new("Uncharted 4", "Acción", "Naughty Dog", "Play Station", 5, 2016, "Español", "Game", @precios)
+    videoj.get_lista_precios()
 end
