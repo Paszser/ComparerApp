073cc1d01b61b8ac623baa93e78b0abe9587f728
Paszser <dbpaszser@gmail.com>
2021-11-24 17:02:35 +0100

    Modifica Rakefile para incluir la ejecución de los test y modifica iv.yaml

--- lib/videojuego.rb
+++ lib/videojuego.rb
@@ -17,6 +17,7 @@ module ComparerApp
         # +valoracion+:: puntuación media de los usuarios
         # +tiendas_distribuidoras+:: lista de tiendas que venden el videojuego
         # +precios+:: lista de elementos clave-valor (nombre de la tienda - precio)
+
         def initialize(nombre, genero, desarrollador, plataformas_disponibles, valoracion, f_lanzamiento, idiomas, tiendas_distribuidoras, precios_videojuego)
             @nombre = nombre
             @genero = genero
@@ -31,22 +32,8 @@ module ComparerApp
         def get_lista_precios()
             precios_videojuego = self.precios_videojuego
             precios_videojuego = precios_videojuego.sort_by{|tienda, precio| precio}.to_h
-            #precios_videojuego.each do |llave, valor|
-                #puts "#{llave}: #{valor}"
-            #end
+
             return precios_videojuego
         end
     end
-
-=begin @precios = {
-        "Game" => 30,
-        "Instant Gaming" => 5, 
-        "PSStore" => 25,
-        "Amazon" => 20
-    }
-
-    videoj = Videojuego.new("Uncharted 4", "Acción", "Naughty Dog", "Play Station", 5, 2016, "Español", "Game", @precios)
-    lista = videoj.get_lista_precios()
-    puts "#{lista}"
-=end
 end
