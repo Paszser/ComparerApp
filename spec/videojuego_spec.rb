dc084b25deefcac55982e040e58e9c0a504d0e3a
Paszser <dbpaszser@gmail.com>
2021-11-30 17:51:24 +0100

    Incluye test para comprobar la funcionalidad del código implementado para resolver #22

--- spec/videojuego_spec.rb
+++ spec/videojuego_spec.rb
@@ -12,8 +12,8 @@ describe 'Videojuego' do
         videoj = ComparerApp::Videojuego.new("Uncharted 4", "Acción", "Naughty Dog", "Play Station", 5, 2016, "Español", "Game", @precios)
         lista = videoj.get_lista_precios()
         all_values = lista.values
-        
         index = 0
+        
         lista.each do |(llave, valor)|
             if index < (lista.length-1) 
                 expect(valor).to be <= all_keys[index+1]
